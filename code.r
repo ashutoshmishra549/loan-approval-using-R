8feb9e1a65167ccd6ba520bbc1febf840de64dd1
ashutoshmishra549 <mishraashutosh549@gmail.com>
2018-10-28 23:57:38 +0530

    Create code.r

---
+++ code.r
@@ -0,0 +1,17 @@
+credit<-read.csv("C:\\Users\\Ashutosh\\Desktop\\credit.csv")
+View(credit)
+set.seed(2)
+id<-sample(2,nrow(credit),prob =c(0.7,0.3),replace=TRUE)
+credit_train<-credit[id==1,]
+credit_test<-credit[id==2,]
+library(randomForest)
+credit$decision<-as.factor(credit$decision)
+credit_train$decision<-as.factor(credit_train$decision)
+bestmtry<-tuneRF(credit_train,credit_train$decision,stepFactor = 1.2,improve = 0.01,trace = T,plot = T)
+credit_forest<-randomForest(decision~.,data=credit_train)
+credit_forest
+varImpPlot(credit_forest)
+pred<-predict(credit_forest,newdata = credit_test,type = "class")
+pred
+library(caret)
+confusionMatrix(table(pred,credit_test$decision))
